ad90dfea797cc78181bb8fa14ab89af5b0cfed08
collective1x3-5242 <collective1x3@community-13.com>
2025-10-09 18:51:08 +0200

    Use JS Tailwind config for build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./lib/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -12,5 +12,3 @@ const config: Config = {
   },
   plugins: [],
 };
-
-export default config;
